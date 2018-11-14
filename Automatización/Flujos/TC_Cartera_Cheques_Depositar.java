aa31e4d7d88d34d7b2845c6ea1a2cfd074ba0133
manuelfvalless <manuelfvalless@gmail.com>
2018-11-14 16:57:20 -0300

    Actualizar

---
+++ Automatización/Flujos/TC_Cartera_Cheques_Depositar.java
@@ -0,0 +1,87 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TCCarteraChequesDepositar {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://54.235.81.157/carteragx15/seclogin.aspx";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTCCarteraChequesDepositar() throws Exception {
+    driver.get(baseUrl + "/carteragx15/seclogin.aspx");
+    driver.findElement(By.id("vUSUARIONOMBRE")).clear();
+    driver.findElement(By.id("vUSUARIONOMBRE")).sendKeys("mvalles");
+    driver.findElement(By.id("vUSUARIOPASSWORD")).clear();
+    driver.findElement(By.id("vUSUARIOPASSWORD")).sendKeys("Mvll18.");
+    driver.findElement(By.id("BTNENTER")).click();
+    driver.findElement(By.xpath("//tr[6]/td/table/tbody/tr/td[2]/p/span/a")).click();
+    new Select(driver.findElement(By.id("vDYNAMICFILTERSSELECTOR1"))).selectByVisibleText("Estado");
+    new Select(driver.findElement(By.id("vCHEQUEESTADO1"))).selectByVisibleText("EN CARTERA");
+    driver.findElement(By.id("vSELECTED_0001")).click();
+    driver.findElement(By.id("DEPOSITAR")).click();
+    boolean depositar = isElementPresent(By.xpath("//td"));
+    driver.findElement(By.id("vCHEQUEFECHADEPOSITO")).clear();
+    driver.findElement(By.id("vCHEQUEFECHADEPOSITO")).sendKeys("14/11/18");
+    driver.findElement(By.id("vCHEQUENROCOMPROBANTE")).clear();
+    driver.findElement(By.id("vCHEQUENROCOMPROBANTE")).sendKeys("14112018");
+    driver.findElement(By.id("BTNENTER")).click();
+    boolean chequeDepositado = isElementPresent(By.id("DEPOSITAR"));
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
