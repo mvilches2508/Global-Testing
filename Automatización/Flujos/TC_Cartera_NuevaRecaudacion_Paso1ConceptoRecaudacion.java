73977bac0c2fc26eecda0e5033c28c72fcf52c0b
manuelfvalless <manuelfvalless@gmail.com>
2018-11-14 13:10:13 -0300

    Actualizar

---
+++ Automatización/Flujos/TC_Cartera_NuevaRecaudacion_Paso1ConceptoRecaudacion.java
@@ -0,0 +1,88 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TCCarteraNuevaRecaudacionPaso1ConceptoRecaudacion {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://54.235.81.157/carteragx15/seclogin.aspx";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTCCarteraNuevaRecaudacionPaso1ConceptoRecaudacion() throws Exception {
+    driver.get(baseUrl + "/carteragx15/seclogin.aspx");
+    driver.findElement(By.id("vUSUARIONOMBRE")).clear();
+    driver.findElement(By.id("vUSUARIONOMBRE")).sendKeys("mvalles");
+    driver.findElement(By.id("vUSUARIOPASSWORD")).clear();
+    driver.findElement(By.id("vUSUARIOPASSWORD")).sendKeys("Mvll18.");
+    driver.findElement(By.id("BTNENTER")).click();
+    driver.findElement(By.xpath("//tr[2]/td/table/tbody/tr/td[2]/p/span/a")).click();
+    new Select(driver.findElement(By.id("vCREESTADOCONTRATO"))).selectByVisibleText("VIGENTE");
+    driver.findElement(By.id("BTNBUSCAR")).click();
+    driver.findElement(By.id("vSELECCIONAR_0008")).click();
+    new Select(driver.findElement(By.id("vRECAUDACIONOTROSID"))).selectByVisibleText("Otros");
+    driver.findElement(By.id("vGRIDCUOTASSELECTED_0001")).click();
+    driver.findElement(By.id("vGRIDCUOTASSELECTED_0002")).click();
+    driver.findElement(By.id("BTNSIGUIENTE")).click();
+    driver.findElement(By.id("vCREFCHPAGOREAL")).clear();
+    driver.findElement(By.id("vCREFCHPAGOREAL")).sendKeys("14/11/18");
+    driver.findElement(By.id("BTNENTER")).click();
+    boolean proceso = isElementPresent(By.id("TEXTBLOCKTITLE"));
+    driver.findElement(By.id("IMAGE2_MPAGE")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
