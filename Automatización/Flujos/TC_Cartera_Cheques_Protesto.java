00ec50fa64239e8400a7a9a2446c48565330d257
manuelfvalless <manuelfvalless@gmail.com>
2018-11-15 17:20:48 -0300

    Actualizar

---
+++ Automatización/Flujos/TC_Cartera_Cheques_Protesto.java
@@ -0,0 +1,86 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TCCarteraChequesProtesto {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://54.235.81.157/carteragx15/seclogin.aspx";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testTCCarteraChequesProtesto() throws Exception {
+    driver.get(baseUrl + "/carteragx15/seclogin.aspx");
+    driver.findElement(By.id("vUSUARIONOMBRE")).clear();
+    driver.findElement(By.id("vUSUARIONOMBRE")).sendKeys("mvalles");
+    driver.findElement(By.id("vUSUARIOPASSWORD")).clear();
+    driver.findElement(By.id("vUSUARIOPASSWORD")).sendKeys("Mvll18.");
+    driver.findElement(By.id("BTNENTER")).click();
+    driver.findElement(By.xpath("//tr[6]/td/table/tbody/tr/td[2]/p/span/a")).click();
+    new Select(driver.findElement(By.id("vDYNAMICFILTERSSELECTOR1"))).selectByVisibleText("Estado");
+    driver.findElement(By.xpath("//option[6]")).click();
+    new Select(driver.findElement(By.id("vCHEQUEESTADO1"))).selectByVisibleText("DEPOSITADO");
+    String nroCheque = driver.findElement(By.xpath("//td[8]/span/a")).getText();
+    driver.findElement(By.id("vPROTESTO_0001")).click();
+    new Select(driver.findElement(By.id("vMOTIVOPROTESTOID"))).selectByVisibleText("CADUCADO");
+    boolean protestarCheque = isElementPresent(By.xpath("//td"));
+    driver.findElement(By.id("BTNENTER")).click();
+    String nroCheque2 = driver.findElement(By.xpath("//td[8]/span/a")).getText();
+    driver.findElement(By.id("IMAGE2_MPAGE")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
